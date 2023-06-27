6b8c9df0b603b633bd55262010748e4fa952b3bb
LEENeal <fod14@hanmail.net>
2023-06-27 16:10:47 +0900

    interceptor class name make uppercase

--- src/main/java/project/com/hotplace/interceptor/Interceptor.java
+++ src/main/java/project/com/hotplace/interceptor/Interceptor.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class interceptor extends HandlerInterceptorAdapter {
+public class Interceptor extends HandlerInterceptorAdapter {
 
 //	@Autowired
 //	HttpSession session;

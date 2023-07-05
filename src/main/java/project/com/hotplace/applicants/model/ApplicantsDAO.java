afe16e5f396207686033debfe51955c84dcb87cf
korno1 <jml6534@naver.com>
2023-07-05 09:23:27 +0900

    pull commit

--- src/main/java/project/com/hotplace/applicants/model/ApplicantsDAO.java
+++ src/main/java/project/com/hotplace/applicants/model/ApplicantsDAO.java
@@ -3,9 +3,9 @@ package project.com.hotplace.applicants.model;
 import java.util.List;
 
 public interface ApplicantsDAO {
-	public List<ApplicantsVO> selectAll();
+	public List<ApplicantsVO> selectAll(ApplicantsVO vo);
 	
 	public int insert(ApplicantsVO vo);
 	public int approve(ApplicantsVO vo);
-	public int delete(ApplicantsVO vo);
+	public int reject(ApplicantsVO vo);
 }

31429ae70176356506841d56bc12911597c9b5d4
Pmy <minyoung51777@naver.com>
2023-07-06 11:47:53 +0900

    userpage par_selectAll commit

--- src/main/java/project/com/hotplace/memberreview/controller/MemberReviewController.java
+++ src/main/java/project/com/hotplace/memberreview/controller/MemberReviewController.java
@@ -1,23 +1,34 @@
 package project.com.hotplace.memberreview.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.slf4j.Slf4j;
+import project.com.hotplace.member.model.MemberVO;
+import project.com.hotplace.member.service.MemberService;
 
 
 @Slf4j
 @Controller
 public class MemberReviewController {
 	
+	@Autowired
+	MemberService service;
+	
 
 	@RequestMapping(value = {"/userpage.do"}, method = RequestMethod.GET)
-	public String userpage() {
+	public String userpage(MemberVO vo, Model model) {
 		log.info("/userpage.do");
 		
+		MemberVO vo2 = service.selectOne(vo);
+		log.info("vo2 outinfo...{}",vo2);
 		
-		return "memberreview/userpage";
+		model.addAttribute("vo2",vo2);
+		
+		return "memberreview/userpage.tiles";
 	}
 	
 	

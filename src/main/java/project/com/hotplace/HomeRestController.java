d33858e795ebb27128a845d49d830101a304c710
ChoMosigi <whdbsqja1@gmail.com>
2023-07-17 17:52:07 +0900

    집 근처 음식점 카테고리 통계 완료

--- src/main/java/project/com/hotplace/HomeRestController.java
+++ src/main/java/project/com/hotplace/HomeRestController.java
@@ -1,7 +1,9 @@
 package project.com.hotplace;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
@@ -49,6 +51,7 @@ public class HomeRestController {
 		
 	    double degreeToKmRatio = 40075.16 / 360.0; // 1도에 해당하는 거리 (단위: km)
        
+	    Map<String, Integer> categoryCountMap = new HashMap<>(); // 카테고리별 count를 저장할 맵
        
 		for(ShopVO shop : vos) {
     	   double locX = shop.getLoc_x();
@@ -59,15 +62,32 @@ public class HomeRestController {
     	   
            double distance = Math.sqrt(Math.pow(deltaX * degreeToKmRatio, 2) + Math.pow(deltaY * degreeToKmRatio, 2));
            
-           shop.setDistance(distance);
-           
            log.info("{}", shop);
     	   log.info("distance:{}", distance);
+           
+           shop.setDistance(distance);
            
            if (distance <= 20.0)
                nearShops.add(shop);
 		}
-
+		
+		// 카테고리별 count 정보를 ShopVO에 추가
+	    for (ShopVO shop : nearShops) {
+	        String[] categories = shop.getCate().split(","); // 카테고리 목록을 쉼표로 분리하여 배열로 변환
+	        
+	        for (String category : categories) {
+                int count = categoryCountMap.getOrDefault(category, 0);
+                categoryCountMap.put(category, count + 1); // 해당 카테고리의 count 증가
+            }
+	    }
+	    
+	    for (Map.Entry<String, Integer> entry : categoryCountMap.entrySet()) {
+	        String category = entry.getKey();
+	        int count = entry.getValue();
+	        log.info("Category: {} - Count: {}", category, count);
+	    }
+		
+		
 		return nearShops;
     }
 }
